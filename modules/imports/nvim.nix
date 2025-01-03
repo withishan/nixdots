5a6a5519ab568d70549253a61dd9a21f2682dc2e
xarwan <xarwan40@gmail.com>
2025-01-03 18:56:53 +0545

    nvim changes

--- modules/imports/nvim.nix
+++ modules/imports/nvim.nix
@@ -1,8 +1,10 @@
 { config, pkgs, lib, ... }: {
   programs.neovim = {
     enable = true;
+    defaultEditor = true;
     plugins = with pkgs.vimPlugins; [
       lualine-nvim
+      catppuccin-nvim
     ];
   };
 }
