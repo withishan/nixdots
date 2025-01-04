e540916127dafd0597effc05ae726f3c2007e358
xarwan <xarwan40@gmail.com>
2025-01-04 14:49:03 +0545

    nixvim changes

--- modules/imports/nixvim.nix
+++ modules/imports/nixvim.nix
@@ -1,7 +1,23 @@
 { self, ... }: {
   programs.nixvim = {
     enable = true;
+
+    vimAlias = true;
+
+    globals = {
+      mapleader = " ";
+    };
+
+    opts = {
+      relativenumber = true;
+      number = true;
+      expandtab = true;
+      shiftwidth = 2;
+      tabstop = 2;
+    };
+
     colorschemes.kanagawa.enable = true;
+
     plugins = {
       lualine.enable = true;
     };
