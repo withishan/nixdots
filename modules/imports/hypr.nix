2ecb077bb309f8df5624432e300c8b996b6316b1
withishan <404ishan@gmail.com>
2025-03-09 18:33:31 +0545

    uh..

--- modules/imports/hypr.nix
+++ modules/imports/hypr.nix
@@ -30,7 +30,7 @@
       };
 
       decoration = {
-        rounding = 15;
+        rounding = 20;
         active_opacity = 1.0;
         inactive_opacity = 1.0;
 
@@ -39,7 +39,7 @@
           range = 52;
           offset = "3 3";
           render_power = 5;
-          color = "rgba(0, 0, 0, 0.6)";
+          color = "rgba(0, 0, 0, 0.5)";
         };
 
         blur = {
