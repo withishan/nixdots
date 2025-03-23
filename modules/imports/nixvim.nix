e3782d0e49643acbd7d07f75a5a2e347db6950ae
withishan <404ishan@gmail.com>
2025-03-23 08:54:43 +0545

    nixvim changes

--- modules/imports/nixvim.nix
+++ modules/imports/nixvim.nix
@@ -83,48 +83,17 @@
             }, 
           },
           sections = {
-            lualine_a = { 
-              { 
-                "mode", 
-                fmt = function(s) return mode_map[s] or s end, 
-                separator = { 
-                  left = " ", 
-                  right = "" 
-                } 
-              } 
+            lualine_a = { { "mode", fmt = function(s) return mode_map[s] or s end, separator = { left = " ", right = "" } } },
+            lualine_b = { { "branch", icon = "" }, },
+            lualine_c = { "filename" },
+            lualine_x = { { "diff", symbols = { added = " ", modified = " ", removed = " " }, colored = false, },
             },
-            lualine_b = { 
-              { 
-                "branch", 
-                icon = "" 
-              },
-            },
-            lualine_c = {
-              "filename"
-            },
-            lualine_x = {
-            {
-					      "diff",
-					      symbols = { added = " ", modified = " ", removed = " " },
-					      colored = false,
-					    },
-            },
-            lualine_y = {
-                {
-                  "diagnostics",
-                  symbols = { error = " ", hint = " ", info = " ", warn = " " },
-                  update_in_insert = true,
-                },
-                { "location", icon = "" },
-            },
-            lualine_z = {
-              {
+            lualine_y = { { "diagnostics", symbols = { error = " ", hint = " ", info = " ", warn = " " }, update_in_insert = true, }, },
+            lualine_z = { {
                 function()
                   return os.date("%-I:%-M %p")
                 end,
-                separator = { left = "", right = " " },
-              }
-            },
+                separator = { left = "", right = " " }, } },
           },
       })
     '';
